58a3087183dad1f0060c0a717f1518ef1d9f09f5
Thomas McNally <thomas_mcnally_tmg@whirlpool.com>
2019-07-24 19:17:17 -0400

    Adding

--- src/main/java/portfolio/controller/HomepageController.java
+++ src/main/java/portfolio/controller/HomepageController.java
@@ -4,14 +4,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.web.ErrorController;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.thymeleaf.util.StringUtils;
-import portfolio.util.WoL;
 
 import javax.servlet.http.HttpServletRequest;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * Created by missionary on 2016-03-06.
@@ -29,6 +29,8 @@ public class HomepageController implements ErrorController {
 
     private static final String PLEX_SERVERNAME = "plex";
     private static final String PLEX_REDIRECT = "redirect:http://plexserver.tkmcnally.com:32400/web/index.html";
+    private static final String PLEX_WAKEUP_PATH = "/wakeplex";
+    private static final String PLEX_WAKEUP_PORT = ":3000";
 
     @Value("${plex.server.ip}")
     private String PLEX_SERVER_IP;
@@ -40,8 +42,16 @@ public class HomepageController implements ErrorController {
     public String getHomepage(HttpServletRequest request, Model model) {
         final String serverName = request.getServerName().split("\\.")[0];
         if(StringUtils.equals(PLEX_SERVERNAME, serverName)) {
-            WoL.sendPacket(PLEX_SERVER_IP, PLEX_SERVER_MAC);
-            logger.info("ServerNAME: " + serverName);
+            try {
+                URL url = new URL(PLEX_SERVER_IP + PLEX_WAKEUP_PORT + PLEX_WAKEUP_PATH);
+                HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                con.setRequestMethod("GET");
+                con.getResponseCode();
+            } catch (Exception e) {
+                logger.error("Error sending GET request to " + PLEX_SERVER_IP, e);
+            }
+
+            logger.info("Sending GET request to: " + PLEX_SERVER_IP + PLEX_WAKEUP_PATH);
             return PLEX_REDIRECT;
         }
         return INDEX_PAGE;
